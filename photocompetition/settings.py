cedecb01755c2fb388ba730d09dddd3cf1bbf526
Exitus <exitusepsilon@gmail.com>
2021-11-10 21:22:43 +0300

    Added .env file with configuration.

--- photocompetition/settings.py
+++ photocompetition/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jn8w9*%^mn-axx%vrr6vtqyqb@$0ia=k0v#y1kjm-3z0k0cso*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[])
 
 
 # Application definition
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'photocompetition.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -83,11 +84,11 @@ WSGI_APPLICATION = 'photocompetition.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'photocomp',
-        'USER': 'djangophoto',
-        'PASSWORD': '1488',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DATABASES_NAME'),
+        'USER': config('DATABASES_USER'),
+        'PASSWORD': config('DATABASES_PASSWORD'),
+        'HOST': config('DATABASES_HOST', default='localhost'),
+        'PORT': config('DATABASES_PORT', default='5432')
     }
 }
 
